27dd9cfd4d0c25d7b179015f0c37b1b743ef6421
J.R Perez <jrbaprz@gmail.com>
2022-12-20 15:15:03 -0500

    Added hero section and categories

--- components/header.js
+++ components/header.js
@@ -3,26 +3,28 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <section>
-        <div className="w-screen bg-gray-200 p-1">
-          <span className="text-xs flex justify-center items-center">
-            Get your order in by 12/25! See shipping guide for more details.
-          </span>
-        </div>
-      </section>
+      <nav className="fixed z-50">
+        <section>
+          <div className="w-screen bg-gray-200 p-1">
+            <span className="text-xs flex justify-center items-center">
+              Get your order in by 12/25! See shipping guide for more details.
+            </span>
+          </div>
+        </section>
 
-      <nav className="grid grid-cols-3 justify-center items-center w-screen p-3">
-        <div className="flex mr-auto gap-2">
-          <div>Menu</div>
-          <div>Search</div>
-        </div>
-        <div className="text-center text-xl font-extrabold tracking-widest">
-          COTN
-        </div>
-        <div className="flex ml-auto gap-2">
-          <div>Help</div>
-          <div>Cart</div>
-        </div>
+        <section className="bg-white grid grid-cols-3 justify-center items-center w-screen p-3">
+          <div className="flex mr-auto gap-2">
+            <div>Menu</div>
+            <div>Search</div>
+          </div>
+          <div className="text-center text-xl font-extrabold tracking-widest">
+            COTN
+          </div>
+          <div className="flex ml-auto gap-2">
+            <div>Help</div>
+            <div>Cart</div>
+          </div>
+        </section>
       </nav>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
 import React from "react";
+import Image from "next/image";
+import Hero from "../assets/images/hero.jpg";
 
 const index = () => {
-  return <></>;
+  return (
+    <>
+      <section className="overflow-hidden flex items-center justify-center h-screen">
+        <div className="h-screen">
+          <Image
+            src={Hero}
+            alt="Photo of clothes"
+            className="z-10 responsive object-cover"
+            fill
+            priority
+          />
+        </div>
+        <div className="z-20 container mx-auto flex flex-col items-center text-center">
+          <h1 className="text-5xl text-white tracking-wide">
+            The Perfect Essentials
+          </h1>
+          <div className="flex justify-center mt-8 gap-6">
+            <div className="border w-32 py-1 bg-black bg-opacity-50 hover:bg-white duration-300 group hover:text-white">
+              <span className="text-xs text-white uppercase font-akkurat tracking-widest group-hover:text-black">
+                Shop Mens
+              </span>
+            </div>
+
+            <div className="border w-32 py-1 bg-black bg-opacity-50 hover:bg-white duration-300 group hover:text-white">
+              <span className="text-xs text-white uppercase font-akkurat tracking-widest group-hover:text-black">
+                Shop Womens
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="p-12 gap-12 grid">
+        <div className="container mx-auto flex flex-col items-center text-center border-black border-[1px] p-4">
+          <h1 className="tracking-wide">Menswear</h1>
+        </div>
+        <div className="container mx-auto flex flex-col items-center text-center border-black border-[1px] p-4">
+          <h1 className="tracking-wide">Menswear</h1>
+        </div>
+        <div className="container mx-auto flex flex-col items-center text-center border-black border-[1px] p-4">
+          <h1 className="tracking-wide">Menswear</h1>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default index;
